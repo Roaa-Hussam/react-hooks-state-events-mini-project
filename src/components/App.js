5b7036aedc442a2c55ee48ab1418f67167fcc1ee
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-23 17:18:25 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
+
+
 function App() {
+
+  const [tasks, setTasks] = useState(TASKS)
+
+  const [selectedCatagory, setSelectedCatagory] = useState('All')
+
+  function handleCatagory(event) {
+    setSelectedCatagory(event)
+  }
+
+  function onTaskFormSubmit(newItem) {
+    setTasks([...tasks, newItem])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCatagory={selectedCatagory} handleCatagory={handleCatagory} />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit} />
+      <TaskList tasks={tasks} setTasks={setTasks} selectedCatagory={selectedCatagory} />
     </div>
   );
 }
